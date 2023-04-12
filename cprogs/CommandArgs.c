5c3f0f52962a1c354804438c61a41a6f26003c3d
Dawson White <Dawsonwhi@gmail.com>
2023-04-12 02:03:47 +0000

    trying to figure this out

---
+++ cprogs/CommandArgs.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(int n, char* args[]) {
+    int i;
+    printf("Number of args: %d\n", n);
+    for (i = 0; i < n; i++) {
+    printf("%d %s\n",i,args[i]);
+    }
+}

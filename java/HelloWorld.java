9787d09caf7a03c97830afe14a788165f6ba4b5b
Dawson White <Dawsonwhi@gmail.com>
2023-04-12 02:50:05 +0000

    changed

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,6 +1,18 @@
+/*
+    The Hello World Program in Java
+ */
+
+/* Java Math library */
+import java.lang.Math;
+
+/* define a HelloWorld class */
 class HelloWorld {
-    public static void main (String[] args)
-    {
-    System.out.print("Hello World!");
-    }
+
+  /* main method definition: */
+  public static void main(String[] args){
+
+   System.out.println("Hello World");
+   System.out.println("sqrt(4) is "
+         + Math.sqrt(4));
+  }
 }

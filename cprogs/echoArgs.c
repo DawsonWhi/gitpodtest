3c3cbd5b6657f4b5c83af41941af042ecfb59e79
Dawson White <Dawsonwhi@gmail.com>
2023-04-18 21:07:33 +0000

    Made echoArgs not crash added addTwo

--- cprogs/echoArgs.c
+++ cprogs/echoArgs.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int n, char* args[]) {
 	printf("number of arguments: %d\n", n);
-    for(int i = 0; i < args.length; i++)
+    for(int i = 0; i < sizeof(args[i]); i++)
     {
-        
+        printf("%d %s\n", i, args[i]);
     }
 }
